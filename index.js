424a8f76629654284ac3d6d420dbc0a653634bef
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 22:15:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ class Route {
   }
   estimatedTime(peak = false) {
     if (peak === false) {
-      return this.blocksTravelled() * 3;
+      return this.blocksTravelled() / 3;
     } else {
-      return this.blocksTravelled() * 2;
+      return this.blocksTravelled() / 2;
     }
   }
 }
