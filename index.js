4a643c8b8c41f1a12e51ba84846896272296e194
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 22:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Route {
   }
   blocksTravelled() {
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-    return
+    console.log(this.beginningLocation,this.endingLocation);
   }
 }
 
