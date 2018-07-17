9d4e66fffd09d8f675d98c23688a406c516fee66
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 21:43:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Driver {
     this.startDate = startDate;
   }
   startDate() {
-    date = new Date.now();
+    date = new Date(Date.now);
     return date;
   };
   yearsExperienceFromBeginningOf(year) {
