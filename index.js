47b8fdd149e8d40f7c7dbb7c26c55a50d1d5b01c
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 21:57:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Driver {
   constructor(name, date) {
     this.name = name;
-    this.date = date;
+    this.startDate = new Date(date);
   }
   startDate() {
     this.startDate = new Date(this.date);
