43879f05d8826d035584bb3e19228ea5513d3555
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 21:48:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 class Driver {
-  constructor(name, startDate) {
+  constructor(name, date) {
     this.name = name;
-    this.startDate = startDate;
+    this.date = date;
   }
   startDate() {
-    const date = new Date('December 17, 1995 03:24:00');
+    const date = new Date(this.date);
     return date;
   };
   yearsExperienceFromBeginningOf(year) {
