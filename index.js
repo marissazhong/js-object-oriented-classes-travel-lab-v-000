0a55a43a180a7a337b52f59415cfe985f2c27e11
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 22:00:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ class Driver {
     this.name = name;
     this.startDate = new Date(date);
   }
-
   yearsExperienceFromBeginningOf(year) {
     return year-this.startDate.getFullYear();
   };
@@ -13,6 +12,10 @@ class Route {
   constructor(beginningLocation, endingLocation) {
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
+  }
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    return
   }
 }
 
